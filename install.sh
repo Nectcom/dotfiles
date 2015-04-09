82019c5ac67e4b9bbe86b8503063a42673b3fe50
gifuroboken <gifuroboken@GNCTRobotClub.GNCT>
2015-04-09 23:15:46 +0900

    commit No.1 delete files.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo 'Thank you for install my DotFiles!'
+echo 'This is installed list ...'
+
+ln -s ./vimmod ~/.vim
+ln -s .vimrc ~/.vimrc
+ln -s .bashrc ~/.bashrc
+ln -s .bash_profile ~/.bash_profile
+
+echo '.vim \n.vimrc \n.bashrc \n.bash_profile'
+echo 'nothing matter.'
